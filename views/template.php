a59da0466b7375dbf440ce21b5c224b2839caac3
Asafe Marques <asafebymarques@icloud.com>
2023-07-11 07:10:18 -0300

    changed

--- views/template.php
+++ views/template.php
@@ -113,7 +113,167 @@
     </div>
     <!-- End Hero Banner -->
     <!-- Start About Us -->
-    <?php $this->loadViewInTemplate($viewName, $viewData); ?>
+    <section class="about-us-bg position-relative c-home-about">
+        <div class="container">
+            <?php $this->loadViewInTemplate($viewName, $viewData); ?>
+        </div>
+    </section>
+
+    <section class="about-couple">
+        <div class="container couple-container">
+            <img src="<?=BASE_URL?>assets/images/object/couple_shape.png" alt="" class="coupleshape">
+            <div class="c-flex-space-between m-flex">
+                <div class="couple-left">
+                    <h6 class="font-raleway">A Noiva</h6>
+                    <h5 class="font-playfair">Hevelin</h5>
+                    <p>"Gratidão a Todos vocês  por compartilharem conosco esse momento tao importante e especial.<br>Grande Beijo"</p>
+                </div>
+                <div class="couple-center">
+                    <div class="ac-main-img">
+                        <img class="about-center" src="<?=BASE_URL?>assets/images/home/image1.jpeg" alt="">
+                    </div>
+                </div>
+                <div class="couple-right">
+                    <h6 class="font-raleway">O Noivo</h6>
+                    <h5 class="font-playfair">Asafe</h5>
+                    <p>"Agradeço a todos que fizeram parte da minha história e vai ser uma honra que você faça parte da nossa história nesse momento, gostaria de agradecer a Deus e aos nossos pais..."</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="c-home-photocarousel">
+        <div class="section photo-carousel-wrapper p-0">
+            <div class="main-title text-center position-relative">
+                <span class="font-playfair">Nossos Momentos</span>
+                <h5 class="font-raleway">Alguns de nossos momentos</h5>
+            </div>
+            <div class="container-fluid c-container-padding">
+                <div id="gallery-slides" class="owl-carousel owl-theme" data-aos="zoom-in-up">
+                    <div class="item position-relative">
+                        <img src="<?=BASE_URL?>assets/images/home/image1.jpeg" alt="photo">
+                    </div>
+                    <div class="item position-relative">
+                        <img src="<?=BASE_URL?>assets/images/home/image2.jpeg" alt="photo">
+                    </div>
+                    <div class="item position-relative">
+                        <img src="<?=BASE_URL?>assets/images/home/image3.jpeg" alt="photo">
+                    </div>
+                    <div class="item position-relative">
+                        <img src="<?=BASE_URL?>assets/images/home/image4.jpeg" alt="photo">
+                    </div>
+                    <div class="item position-relative">
+                        <img src="<?=BASE_URL?>assets/images/home/image5.jpeg" alt="photo">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="celebration-time-sec position-relative">
+        <div class="main-title more-text text-center position-relative">
+            <span class="font-playfair">Quando & Onde</span>
+            <h5 class="font-raleway">Celebração Inicia</h5>
+        </div>
+        <div class="container-fluid c-container-padding celebration-space-adjust">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="rsvp-full-container about-single-content rsvp-adust">
+                        <div class="rsvp-icon-contents">
+                            <div class="rsvp-bordered">
+                                <img src="<?=BASE_URL?>assets/images/elements-images/svg/rsvp-icon-1.svg" alt="icon">
+                            </div>
+                        </div>
+                        <div>
+                            <h6 class="font-playfair rsvp-single-title">Casamento Civil</h6>
+                            <div class="about-single-sep"></div>
+                        </div>
+                        <p class="text-center rsvp-box-desc-first">Sábado, 23 Setembro 2023 <br>10:00 – 10:20 PM</p>
+                        <p class="text-center rsvp-box-desc-last">Cartório Civil Mogi das Cruzes - R. Olegário Paiva, 186 - Centro, Mogi das Cruzes - SP, 08780-040</p>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="rsvp-full-container about-single-content rsvp-adust">
+                        <div class="rsvp-icon-contents">
+                            <div class="rsvp-bordered">
+                                <img src="<?=BASE_URL?>assets/images/elements-images/svg/rsvp-icon-2.svg" alt="icon">
+                            </div>
+                        </div>
+                        <div>
+                            <h6 class="font-playfair rsvp-single-title">Festa de Casamento</h6>
+                            <div class="about-single-sep"></div>
+                        </div>
+                        <p class="text-center rsvp-box-desc-first">Sábado, 23 Setembro 2023 <br>12:00 – 16:00 PM</p>
+                        <p class="text-center rsvp-box-desc-last">Boi Nobre - Assai mogilar - Av. Pref. Carlos Ferreira Lopes, 600 - Box 30 - Vila Mogilar, Mogi das Cruzes - SP, 08773-490</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="c-about-progress-top event-services-adjust">
+        <div class="c-flex about-progress-inner">
+            <div class="progress-left about-progress">
+                <div class="about-getting-married">
+                    <h4>Festa de Casamento</h4>
+                    <h6 class="text-brown">Você é muito bem vindo!</h6>
+                    <p>
+                        Queremos agradecer desde já a sua participação nesse momento tão especial em nossas vidas. E, mediante disso, planejamos a nossa festa de casamento no Boi nobre em Mogi das Cruzes localizado dentro do Assai Mogilar.
+                        <br>
+                    </p>
+                    <p class="font-weight-bold">"Nós apreciaríamos muito a sua presença no nosso casamento. Por favor, esteja ciente de que todos os custos são de responsabilidade de cada convidado."</p>
+                </div>
+            </div>
+            <div class="progress-main-wrapper">
+            </div>
+        </div>
+    </section>
+
+    <section class="cta-textonly-sec-orange back-lightgrey">
+        <div class="container-fluid c-container-padding">
+            <div class="main-title more-text text-center position-relative">
+                <span class="font-playfair">Lista de Presentes</span>
+                    <h5 class="font-raleway">Casamento</h5>
+            </div>
+            <div class="flex-wrapper row">
+                    <div class="col-md-12">
+                    <div class="accordion orange-accordions" id="orangeAccordionExample" data-aos="fade-up">
+                        <div class="accordion-item">
+                            <h6 class="accordion-header" id="headingOneorange">
+                                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOneorange" aria-expanded="true" aria-controls="collapseOneorange">
+                                    Havan
+                                </button>
+                            </h6>
+                            <div id="collapseOneorange" class="accordion-collapse collapse show" aria-labelledby="headingOneorange" data-bs-parent="#orangeAccordionExample">
+                                <div class="accordion-body">
+                                    <p class="moments-para">Segue a nossa lista de presentes da Havan</p>
+                                    <a href="https://lista.havan.com.br/Convidado/ItensListaPresente/690956" target="_blank">Ir para Havan</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h6 class="accordion-header" id="headingTwoorange">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwoorange" aria-expanded="false" aria-controls="collapseTwoorange">
+                                    Casas Bahia
+                                </button>
+                            </h6>
+                            <div id="collapseTwoorange" class="accordion-collapse collapse" aria-labelledby="headingTwoorange" data-bs-parent="#orangeAccordionExample">
+                                <div class="accordion-body">
+                                    <p class="moments-para">Segue a nossa lista de presentes da Casas Bahia</p>
+                                    <a href="https://listas.casasbahia.com.br/asafeehevelin" target="_blank">Ir para Casas Bahia</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <div class="map-sec">
+        <div class="map-overlay"></div>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15255.61890598669!2d-46.18880444903292!3d-23.512999685793826!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cdd82239aa11f1%3A0xed02bdb0a723c24d!2sBoi%20Nobre%20churrascaria!5e0!3m2!1spt-BR!2sbr!4v1688778301964!5m2!1spt-BR!2sbr"
+            width="100" height="450" style="border: 0; width: 100%;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
 
     <footer class="footer-bg">
         <div class="footer-main" data-aos="zoom-in">
