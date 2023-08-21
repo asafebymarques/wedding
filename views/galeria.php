12a51e040e331ba6f99d36fc19a763c13c72ca06
Asafe Marques <asafebymarques@icloud.com>
2023-08-21 14:04:52 -0300

    Fixed gallery

--- views/galeria.php
+++ views/galeria.php
@@ -82,3 +82,114 @@
         </div>
     </div>
     <!-- End Groom Block -->
+     <!-- Start Groom Block -->
+ <div class="p-0 section">
+        <div class="masonary-content">
+            <div class="masonary-item1" data-aos="flip-left">
+                <a class="gallery-item popup-image" href="<?=BASE_URL?>assets/images/home/image14.jpeg">
+                    <img class="w-100 masonary-image img-responsive" alt="gallery-img" src="<?=BASE_URL?>assets/images/home/image14.jpeg">
+                </a>
+            </div>
+            <div class="masonary-item2" data-aos="flip-left">
+                <a class="gallery-item popup-image" href="<?=BASE_URL?>assets/images/home/image15.jpeg">
+                    <img class="w-100 masonary-image img-responsive" alt="gallery-img" src="<?=BASE_URL?>assets/images/home/image15.jpeg">
+                </a>
+            </div>
+            <div class="masonary-item3" data-aos="flip-left">
+                <a class="gallery-item popup-image" href="<?=BASE_URL?>assets/images/home/image16.jpeg">
+                    <img class="w-100 masonary-image img-responsive" alt="gallery-img" src="<?=BASE_URL?>assets/images/home/image16.jpeg">
+                </a>
+            </div>
+            <div class="masonary-item4" data-aos="flip-left">
+                <a class="gallery-item popup-image" href="<?=BASE_URL?>assets/images/home/image17.jpeg">
+                    <img class="w-100 masonary-image img-responsive" alt="gallery-img" src="<?=BASE_URL?>assets/images/home/image17.jpeg">
+                </a>
+            </div>
+            <div class="masonary-item5" data-aos="flip-left">
+                <a class="gallery-item popup-image" href="<?=BASE_URL?>assets/images/home/image18.jpeg">
+                    <img class="w-100 masonary-image img-responsive" alt="gallery-img" src="<?=BASE_URL?>assets/images/home/image18.jpeg">
+                </a>
+            </div>
+            <div class="masonary-item6" data-aos="flip-left">
+                <a class="gallery-item popup-image" href="<?=BASE_URL?>assets/images/home/image19.jpeg">
+                    <img class="w-100 masonary-image img-responsive" alt="gallery-img" src="<?=BASE_URL?>assets/images/home/image19.jpeg">
+                </a>
+            </div>
+            <div class="masonary-item7" data-aos="flip-left">
+                <a class="gallery-item popup-image" href="<?=BASE_URL?>assets/images/home/image20.jpeg">
+                    <img class="w-100 masonary-image img-responsive" alt="gallery-img" src="<?=BASE_URL?>assets/images/home/image20.jpeg">
+                </a>
+            </div>
+            <div class="masonary-item8" data-aos="flip-left">
+                <a class="gallery-item popup-image" href="<?=BASE_URL?>assets/images/home/image21.jpeg">
+                    <img class="w-100 masonary-image img-responsive" alt="gallery-img" src="<?=BASE_URL?>assets/images/home/image21.jpeg">
+                </a>
+            </div>
+            <div class="masonary-item9" data-aos="flip-left">
+                <a class="gallery-item popup-image" href="<?=BASE_URL?>assets/images/home/image22.jpeg">
+                    <img class="w-100 masonary-image img-responsive" alt="gallery-img" src="<?=BASE_URL?>assets/images/home/image22.jpeg">
+                </a>
+            </div>
+            <div class="masonary-item10" data-aos="flip-left">
+                <a class="gallery-item popup-image" href="<?=BASE_URL?>assets/images/home/image23.jpeg">
+                    <img class="w-100 masonary-image img-responsive" alt="gallery-img" src="<?=BASE_URL?>assets/images/home/image23.jpeg">
+                </a>
+            </div>
+            <div class="masonary-item11" data-aos="flip-left">
+                <a class="gallery-item popup-image" href="<?=BASE_URL?>assets/images/home/image24.jpeg">
+                    <img class="w-100 masonary-image img-responsive" alt="gallery-img" src="<?=BASE_URL?>assets/images/home/image24.jpeg">
+                </a>
+            </div>
+            <div class="masonary-item12" data-aos="flip-left">
+                <a class="gallery-item popup-image" href="<?=BASE_URL?>assets/images/home/image25.jpeg">
+                    <img class="w-100 masonary-image img-responsive" alt="gallery-img" src="<?=BASE_URL?>assets/images/home/image25.jpeg">
+                </a>
+            </div>
+            <div class="masonary-item13" data-aos="flip-left">
+                <a class="gallery-item popup-image" href="<?=BASE_URL?>assets/images/home/image26.jpeg">
+                    <img class="w-100 masonary-image img-responsive" alt="gallery-img" src="<?=BASE_URL?>assets/images/home/image26.jpeg">
+                </a>
+            </div>
+            <div class="masonary-item13" data-aos="flip-left">
+                <a class="gallery-item popup-image" href="<?=BASE_URL?>assets/images/home/image27.jpeg">
+                    <img class="w-100 masonary-image img-responsive" alt="gallery-img" src="<?=BASE_URL?>assets/images/home/image27.jpeg">
+                </a>
+            </div>
+            <div class="masonary-item14" data-aos="flip-left">
+                <a class="gallery-item popup-image" href="<?=BASE_URL?>assets/images/home/image28.jpeg">
+                    <img class="w-100 masonary-image img-responsive" alt="gallery-img" src="<?=BASE_URL?>assets/images/home/image28.jpeg">
+                </a>
+            </div>
+            <div class="masonary-item15" data-aos="flip-left">
+                <a class="gallery-item popup-image" href="<?=BASE_URL?>assets/images/home/image29.jpeg">
+                    <img class="w-100 masonary-image img-responsive" alt="gallery-img" src="<?=BASE_URL?>assets/images/home/image29.jpeg">
+                </a>
+            </div>
+            <div class="masonary-item16" data-aos="flip-left">
+                <a class="gallery-item popup-image" href="<?=BASE_URL?>assets/images/home/image30.jpeg">
+                    <img class="w-100 masonary-image img-responsive" alt="gallery-img" src="<?=BASE_URL?>assets/images/home/image30.jpeg">
+                </a>
+            </div>
+            <div class="masonary-item17" data-aos="flip-left">
+                <a class="gallery-item popup-image" href="<?=BASE_URL?>assets/images/home/image31.jpeg">
+                    <img class="w-100 masonary-image img-responsive" alt="gallery-img" src="<?=BASE_URL?>assets/images/home/image31.jpeg">
+                </a>
+            </div>
+            <div class="masonary-item18" data-aos="flip-left">
+                <a class="gallery-item popup-image" href="<?=BASE_URL?>assets/images/home/image32.jpeg">
+                    <img class="w-100 masonary-image img-responsive" alt="gallery-img" src="<?=BASE_URL?>assets/images/home/image32.jpeg">
+                </a>
+            </div>
+            <div class="masonary-item19" data-aos="flip-left">
+                <a class="gallery-item popup-image" href="<?=BASE_URL?>assets/images/home/image33.jpeg">
+                    <img class="w-100 masonary-image img-responsive" alt="gallery-img" src="<?=BASE_URL?>assets/images/home/image33.jpeg">
+                </a>
+            </div>
+            <div class="masonary-item20" data-aos="flip-left">
+                <a class="gallery-item popup-image" href="<?=BASE_URL?>assets/images/home/image34.jpeg">
+                    <img class="w-100 masonary-image img-responsive" alt="gallery-img" src="<?=BASE_URL?>assets/images/home/image34.jpeg">
+                </a>
+            </div>
+        </div>
+    </div>
+    <!-- End Groom Block -->
